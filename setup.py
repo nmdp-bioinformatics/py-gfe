98b4df8c264035baede73bea58dc73ffa973a64c
Mike Halagan <mhalagan@nmdp.org>
2017-11-13 04:17:04 -0600

    updated version

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ test_requirements = [
 
 setup(
     name='pygfe',
-    version='0.0.1',
+    version='0.0.2',
     description="Python Boilerplate contains all the boilerplate you need to create a Python package.",
     long_description=readme + '\n\n' + history,
     author="Mike Halagan",
