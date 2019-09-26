1f03a3a38d97f63a05d5022f37d5475a45ae2b12
Jason Brelsford <33839713+jbrelsf2-nmdp@users.noreply.github.com>
2019-09-26 10:52:44 -0500

    Updated two packages and setup try catch for util (#26)

--- pygfe/util.py
+++ pygfe/util.py
@@ -1,7 +1,7 @@
 try:
-    from typing import GenericMeta  # python 3.6
+    from typing import Generic as GenericMeta
 except ImportError:
-    # in 3.7, genericmeta doesn't exist but we don't need it
+    # in 3.7, genericmeta doesn't exist as GenericMeta.  Casting Generic as Generic
     class GenericMeta(type): pass
 from datetime import datetime, date
 from six import integer_types, iteritems
