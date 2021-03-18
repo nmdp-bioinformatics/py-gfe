486c025790d0eb568904385784d6562c29fc1039
rsajulga-nmdp <rsajulga@nmdp.org>
2021-03-18 16:49:19 -0500

    bump to 1.1.1

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ test_requirements = [
 
 setup(
     name='py-gfe',
-    version='1.1.0',
+    version='1.1.1',
     description="Python package for converting sequence annotations to gene feature enumerations (GFE).",
     long_description=readme + '\n\n' + history,
     author="CIBMTR",
